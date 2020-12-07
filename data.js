73b0583616c787d995217176bce571d7814c0d13
chrysleremmanuel123 <chrysler.emmanuel@gmail.com>
2020-12-07 22:18:13 +0530

    done

--- data.js
+++ data.js
@@ -1,1 +1,13 @@
+// const { userInfo } = require("os")
 
+
+const userData = function (name, year, qualification) {
+    userInfo = {
+        name: name,
+        Year: year,
+        Qualification: qualification
+    }
+    return userInfo
+}
+
+module.exports = userData;
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const os = require('os')
+const data = require('./data')
+const crypto = require('crypto')
+
+let value = os.userInfo()
+let user = data("Prograd", 2020, "BE")
+const password = crypto.createHmac('sha256', "secret").update('Prograd').digest('hex');
+
+console.log(value);
+console.log(user);
+fs.writeFileSync('message.txt', 'Prograd Details\n')
+fs.appendFileSync('message.txt', `\t\tUserId ->` + value.uid + `\n`)
+fs.appendFileSync('message.txt', `\t\tName ->` + user.name + `\n`)
+fs.appendFileSync('message.txt', `\t\tYear ->` + user.Year + `\n`)
+fs.appendFileSync('message.txt', `\t\tQualification ->` + user.Qualification + `\n`)
+fs.appendFileSync('message.txt', `\t\tUsername -> ` + value.username + ` \n`)
+fs.appendFileSync('message.txt', `\t\tPassword -> ` + password + `\n`)
 
